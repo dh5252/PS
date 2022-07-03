d40de6b67c00e42031b29660ee63bfbaf11a951d
Chiwon <52120998+dh5252@users.noreply.github.com>
2022-07-03 20:02:53 +0900

    [Bronze V] Title: A+B - 3, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10950. A＋B － 3/A＋B － 3.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+
+int main() {
+	
+	int A, B, N;
+
+
+	cin >> N;
+
+	for (int i = 0; i < N; i++) {
+		cin >> A >> B;
+		cout << A + B << endl;
+	}
+
+
+}
