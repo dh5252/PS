6db8480f9c28eef5b9d3fe8b8d46518049516bfb
Chiwon <52120998+dh5252@users.noreply.github.com>
2022-07-17 22:52:24 +0900

    [Bronze V] Title: 와이버스 부릉부릉, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/14645. 와이버스 부릉부릉/와이버스 부릉부릉.cc
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <cmath>
+#include <string>
+#include <utility>
+#include <stdio.h>
+
+using namespace std;
+
+//ios_base::sync_with_stdio(false);
+//cin.tie(NULL);
+//cout.tie(NULL);
+
+
+
+int main() {
+
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int N, K;
+
+	cin >> N >> K;
+
+
+	int a, b;
+	for (int i = 0; i < N; i++) {
+		cin >> a >> b;
+	}
+
+	cout << "비와이";
+	
+
+
+
+
+	return 0;
+}
+
+			
