098052a537004bf27dbb01925ac9d1dd5b621c19
Chiwon <52120998+dh5252@users.noreply.github.com>
2022-07-20 23:44:00 +0900

    [Bronze V] Title: 새로운 시작, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15962. 새로운 시작/새로운 시작.cc
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <cmath>
+#include <string>
+#include <utility>
+#include <stdio.h>
+
+using namespace std;
+
+//ios_base::sync_with_stdio(false);
+//cin.tie(NULL);
+//cout.tie(NULL);
+
+
+
+int main() {
+
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	cout << "파이팅!!";
+
+
+	
+	return 0;
+}
+
+			
