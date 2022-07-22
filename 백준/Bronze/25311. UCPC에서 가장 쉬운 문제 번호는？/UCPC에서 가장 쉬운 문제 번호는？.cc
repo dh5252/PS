bcf96f45f3769d4eb2a4fb4a9396afc4a167f38b
Chiwon <52120998+dh5252@users.noreply.github.com>
2022-07-22 17:38:02 +0900

    [Bronze V] Title: UCPC에서 가장 쉬운 문제 번호는?, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/25311. UCPC에서 가장 쉬운 문제 번호는？/UCPC에서 가장 쉬운 문제 번호는？.cc
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <cmath>
+#include <string>
+#include <utility>
+#include <stdio.h>
+
+using namespace std;
+
+//ios_base::sync_with_stdio(false);
+//cin.tie(NULL);
+//cout.tie(NULL);
+
+
+int main() {
+
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int y;
+
+	cin >> y;
+
+	cout << 'A';
+
+	return 0;
+}
+
+			
