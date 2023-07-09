5f233b609df9997143c76e9a48786b6ac366b221
Chiwon <52120998+dh5252@users.noreply.github.com>
2023-07-09 22:12:42 +0900

    [Gold IV] Title: 숨바꼭질 2, Time: 0 ms, Memory: 3064 KB -BaekjoonHub

---
+++ 백준/Gold/12851. 숨바꼭질 2/숨바꼭질 2.cc
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <vector>
+#include <cstring>
+#include <cmath>
+#include <queue>
+#include <map>
+#include <algorithm>
+#include <utility>
+#include <stack>
+#include <limits.h>
+#include <string>
+
+using namespace std;
+
+int visit[100001][2];
+queue<pair<int, int> > que;
+int dx[3][2] = { {1,1},{-1,1}, {0,2} };
+
+int main()
+{
+    std::cin.tie(nullptr);
+    std::cout.tie(nullptr);
+    std::ios_base::sync_with_stdio(false);
+
+    int N, K; cin >> N >> K;
+
+    que.push({ N, 0 });
+    visit[N][0] = 1;
+    visit[N][1] = 0;
+    while (!que.empty())
+    {
+        pair<int, int> cur = que.front();
+        que.pop();
+
+        if (cur.first == K)
+        {
+            cout << cur.second << "\n" << visit[cur.first][0];
+            break;
+        }
+
+        for (int i = 0; i < 3; ++i)
+        {
+            int rx = dx[i][0] + cur.first * dx[i][1];
+
+            if (rx >= 0 && rx <= 100000)
+            {
+                if (!visit[rx][0])
+                {
+                    visit[rx][1] = cur.second;
+                    visit[rx][0] += visit[cur.first][0];
+                    que.push({ rx, cur.second + 1 });
+                }
+                else if (visit[rx][1] == cur.second)
+                    visit[rx][0] += visit[cur.first][0];
+            }
+        }
+    }
+}
+
