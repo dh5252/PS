659eab6e7b8bd7f9b17bf97537b10312939598ae
Chiwon <52120998+dh5252@users.noreply.github.com>
2023-07-17 16:09:49 +0900

    [Bronze I] Title: 원의 분할, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/16478. 원의 분할/원의 분할.cc
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <cstring>
+#include <queue>
+#include <algorithm>
+#include <map>
+#include <cmath>
+#include <set>
+#include <limits.h>
+#include <list>
+#include <stack>
+
+#define ABS(x) ( ((x)<0)?-(x):(x) ) 
+
+using namespace std;
+
+int main()
+{
+    std::cin.tie(nullptr);
+    std::cout.tie(nullptr);
+    std::ios_base::sync_with_stdio(false);
+
+    int a,b,c; cin >> a >> b >> c;
+    cout.precision(7);
+    cout << (a * c * 1.0) / b * 1.0;
+
+}
