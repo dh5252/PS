66af69c5577321e54f1ebfe452ec1803dd21eb4c
Chiwon <52120998+dh5252@users.noreply.github.com>
2023-08-01 23:55:45 +0900

    [Bronze V] Title: 단어 길이 재기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/2743. 단어 길이 재기/단어 길이 재기.cc
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+#include <cstring>
+#include <algorithm>
+#include <limits.h>
+#include <queue>
+#include <cmath>
+
+#define ABS(x) ( ((x)<0)?-(x):(x) )
+
+using namespace std;
+
+int main()
+{
+    std::cin.tie(nullptr);
+    std::cout.tie(nullptr);
+    std::ios_base::sync_with_stdio(false);
+
+    string str; cin >> str;
+    cout << str.length();
+  
+   
+}
