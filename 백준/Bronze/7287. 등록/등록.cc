0ec44eecf2e65bace8305535ff71a238ab29c34e
Chiwon <52120998+dh5252@users.noreply.github.com>
2023-08-11 01:35:32 +0900

    [Bronze V] Title: 등록, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/7287. 등록/등록.cc
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+#include <cstring>
+#include <algorithm>
+#include <limits.h>
+#include <queue>
+#include <cmath>
+
+#define ABS(x) ( ((x)<0)?-(x):(x) )
+
+using namespace std;
+
+int main()
+{
+    std::cin.tie(nullptr);
+    std::cout.tie(nullptr);
+    std::ios_base::sync_with_stdio(false);
+
+    cout << 235 << "\n" << "dh5252";
+}       
