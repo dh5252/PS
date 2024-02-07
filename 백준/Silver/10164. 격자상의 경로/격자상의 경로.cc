46f1ce6bce1d8d51ed3fb821b352ece1d8cf7939
Chiwon <52120998+dh5252@users.noreply.github.com>
2024-02-07 21:04:47 +0900

    [Silver I] Title: 격자상의 경로, Time: 0 ms, Memory: 2020 KB, Score: 33 point -BaekjoonHub

---
+++ 백준/Silver/10164. 격자상의 경로/격자상의 경로.cc
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int dp[15][15];
+
+int main()
+{
+    cin.tie(NULL);
+    cout.tie(NULL);
+    ios_base::sync_with_stdio(false);
+
+    int N,M,K;
+    cin>>N>>M>>K;
+
+    --K;
+    pair<int, int> k(K / M, K % M);
+
+    dp[0][0] = 1;
+    
+    for (int i = 0 ; i <= k.first ; ++i) {
+        for (int j = 0 ; j <= k.second ; ++j) {
+            if (i > 0) dp[i][j] += dp[i-1][j];
+            if (j > 0) dp[i][j] += dp[i][j-1];
+        }
+    }
+    dp[k.first][k.second] = 0;
+    for (int i = k.first ; i < N ; ++i) {
+        for (int j = k.second ; j < M ; ++j) {
+            if (i > 0) dp[i][j] += dp[i-1][j];
+            if (j > 0) dp[i][j] += dp[i][j-1];
+        }
+    }
+    cout << dp[N-1][M-1];
+
+    return 0;
+}
