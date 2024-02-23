974a729af9720c535eb1f27e886723aa05b78ab5
Chiwon <52120998+dh5252@users.noreply.github.com>
2024-02-23 16:46:49 +0900

    [level 2] Title: 2개 이하로 다른 비트, Time: 32.89 ms, Memory: 27.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/77885. 2개 이하로 다른 비트/2개 이하로 다른 비트.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+long long solv(long long num) {
+    
+    if (num % 2 == 0) return num + 1;
+    
+    long long i = 1;
+    while (((num + 1) & (1L << i)) == 0)
+        i++;
+    
+    num |= (1L << i);
+    num -= (1L << (i - 1));
+    return num;
+}
+
+vector<long long> solution(vector<long long> numbers) {
+    
+    cout << ((long long)1 << 60) << "\n";
+    vector<long long> answer;
+    for (int i = 0 ; i < numbers.size() ; ++i)
+        answer.push_back(solv(numbers[i]));
+    return answer;
+}
